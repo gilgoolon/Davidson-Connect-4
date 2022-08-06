5df70ec5131da6fedfb178831d5b3e36315a79ce
Gil Alpert <alpertgil@gmail.com>
2022-08-06 22:30:46 +0300

    Fifth Commit - added falling animations, validated logic class and integrated win validation through GUI

--- Connect 4/src/com/gil/connect_four/gui/GameGraphicsController.java
+++ Connect 4/src/com/gil/connect_four/gui/GameGraphicsController.java
@@ -1,15 +1,24 @@
 package com.gil.connect_four.gui;
 
 import com.gil.connect_four.logic.Game;
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
 public class GameGraphicsController {
 
@@ -24,9 +33,11 @@ public class GameGraphicsController {
     protected static double height;
     protected static double xLeg;
     protected static double yLeg;
-    protected static final double imaginaryCircleOpc = 1;
+    protected double padding;
+    protected double radius;
+    protected static final double imaginaryCircleOpc = 0.4;
 
-    private Game game;
+    private final Game game;
     private int currentMouseCol;
     private Circle imaginaryCircle;
 
@@ -41,8 +52,8 @@ public class GameGraphicsController {
         height = gamePane.getPrefHeight();
         xLeg = width/Game.COLS;
         yLeg = height/Game.ROWS;
-        double padding =  5.0;
-        double radius = (Math.min(width/Game.COLS,height/Game.ROWS))/2.0-padding;
+        padding =  5.0;
+        radius = (Math.min(width/Game.COLS,height/Game.ROWS))/2.0-padding;
         // draw white circles
         for (int x = 0; x < Game.COLS; x++){
             for (int y = 0; y < Game.ROWS; y++){
@@ -74,14 +85,85 @@ public class GameGraphicsController {
             return;
         currentMouseCol = newCol;
         if (game.isFreeCol(currentMouseCol)){
-            imaginaryCircle.setDisable(false);
+            imaginaryCircle.setOpacity(imaginaryCircleOpc);
             imaginaryCircle.setCenterX(newCol*xLeg + xLeg/2.0);
             imaginaryCircle.setCenterY((Game.ROWS-1-game.firstEmpty(newCol))*yLeg + yLeg/2.0);
-        } else imaginaryCircle.setDisable(true);
+        } else {
+            imaginaryCircle.setOpacity(0);
+            System.out.println("hello");
+        }
     }
 
     @FXML
     void mousePressedBoard(MouseEvent event){
+        if (!game.isFreeCol(currentMouseCol))
+            return;
 
+        fallingAnimation(currentMouseCol, game.firstEmpty(currentMouseCol), game.isRedToMove());
+        game.makeMove(currentMouseCol);
+        if (game.isWin()){
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "Game Over !");
+            a.setHeaderText((game.isRedToMove() ? "The yellow " : "The red ") + "player has won the game.");
+            a.showAndWait();
+        }
+    }
+
+    /**
+     * Place a new circle at the passed index and animate it
+     * @param col represents the column index to place the circle in
+     * @param row represents the row index to place the circle in
+     * @param redColor represents the color of circle to place
+     */
+    private void fallingAnimation(int col, int row, boolean redColor){
+        Circle circle = new Circle(col*xLeg + xLeg/2.0, 0, radius,redColor ? Color.RED : Color.YELLOW);
+        gamePane.getChildren().add(circle);
+        circle.toFront();
+        TranslateTransition trans = new TranslateTransition(Duration.millis(300), circle);
+        trans.setFromY(circle.getLayoutX());
+        trans.setToY((Game.ROWS-1-row)*yLeg + yLeg/2.0);
+        trans.setAutoReverse(false);
+        trans.setCycleCount(1);
+        trans.setInterpolator(new Interpolator() {
+            @Override
+            protected double curve(double v) {
+                return v*v*v; // v^2*g/2 - like physics equation x(t)=a/2*t^2+vt+h
+            }
+        });
+        FadeTransition ft = new FadeTransition(Duration.millis(300), circle);
+        ft.setAutoReverse(false);
+        ft.setCycleCount(1);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.setOnFinished((event) -> {
+            // update imaginary circle
+            Platform.runLater(()->{
+                imaginaryCircle.setFill(game.isRedToMove() ? Color.RED : Color.YELLOW);
+                if (Game.ROWS-1-row-1 >= 0)
+                    imaginaryCircle.setCenterY((Game.ROWS-1-row-1)*yLeg + yLeg/2.0);
+                else imaginaryCircle.setDisable(true);
+            });
+        });
+        // play the transitions
+        ft.play();
+        trans.play();
+        try {
+            playSound("disc_fall.wav");
+        } catch (Exception ignore){}
+
+    }
+
+    /**
+     * Play a sound from a file
+     * @param soundFile represents the filename of the file (with no path to assets)
+     * @throws LineUnavailableException when can't play the sound
+     * @throws IOException when the output was interrupted
+     * @throws UnsupportedAudioFileException when the file isn't of the right type
+     */
+    private void playSound(String soundFile) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        File f = new File("./" + soundFile);
+        AudioInputStream audioIn = AudioSystem.getAudioInputStream(getClass().getResourceAsStream("./assets/" + soundFile));
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioIn);
+        clip.start();
     }
 }
--- Connect 4/src/com/gil/connect_four/logic/Game.java
+++ Connect 4/src/com/gil/connect_four/logic/Game.java
@@ -10,20 +10,10 @@ public class Game {
      */
     public Game(){
         _redToMove = true;
-        _board = new Color[ROWS][COLS];
-        for (int x = 0; x < ROWS; x++)
-            for (int y = 0; y < COLS; y++)
+        _board = new Color[COLS][ROWS];
+        for (int x = 0; x < COLS; x++)
+            for (int y = 0; y < ROWS; y++)
                 _board[x][y] = Color.Empty;
-    }
-
-    /**
-     * Getter function for the color of a specific cell
-     * @param x represents the x index of the cell
-     * @param y represents the y index of the cell
-     * @return the color of the given cell (Color enum - Red(1), Yellow(-1), Empty(0))
-     */
-    public Color get(int x, int y){
-        return _board[x][y];
     }
 
     /**
@@ -35,22 +25,12 @@ public class Game {
     }
 
     /**
-     * Set function to update a certain cell
-     * @param c represents the new color
-     * @param x represents the x index to update
-     * @param y represents the y index to update
-     */
-    private void set(Color c, int x, int y){
-        _board[x][y] = c;
-    }
-
-    /**
      * Make a move on the board - place a new disk and flip the turn
      * @param col represents the column to make the move in (assuming the move is legal)
      */
     public void makeMove(int col){
         int firstEmpty = firstEmpty(col);
-        _board[firstEmpty][col] = _redToMove ? Color.Red : Color.Yellow;
+        _board[col][firstEmpty] = _redToMove ? Color.Red : Color.Yellow;
         _redToMove = !_redToMove;
     }
 
@@ -70,7 +50,7 @@ public class Game {
      */
     public int firstEmpty(int col){
         int i = 0;
-        while (_board[i][col] != Color.Empty && i < ROWS)
+        while (i < ROWS && _board[col][i] != Color.Empty)
             i++;
 
         return i;
@@ -78,31 +58,40 @@ public class Game {
 
     /**
      * Check if the last move resulted in a win for the player who played it
-     * @param lastX represents the x index of the last move
-     * @param lastY represents the y index of the last move
      * @return true if the current board has a win (for the last player who moved), false otherwise
      */
-    public boolean isWin(int lastX, int lastY){
-        /* NEED TO CHECK AND TEST */
-        // vertical check
-        for (int i = 0; i < ROWS-4; i++)
-            if (_board[i][lastY] == _board[i+1][lastY] && _board[i+2][lastY] == _board[i+3][lastY])
-                return true;
-
+    public boolean isWin(){
         // horizontal check
-        for (int i = 0; i < COLS-4; i++)
-            if (_board[lastX][i] == _board[lastX][i+1] && _board[lastX][i] == _board[lastX][i+2] && _board[lastX][i] == _board[lastX][i+3])
-                return true;
+        for (int y = 0; y < ROWS; y++){
+            for (int x = 0; x + 3 < COLS; x++){
+                if (_board[x][y] != Color.Empty && _board[x][y] == _board[x+1][y] && _board[x][y] == _board[x+2][y] && _board[x][y] == _board[x+3][y])
+                    return true;
+            }
+        }
 
-        // diagonal (left-down to right-up) check
-        for (int i = Math.min(3, Math.min(lastX,lastY)); i >= 0 && lastX - i + 3 < ROWS && lastY - i + 3 < COLS; i--)
-            if (_board[lastX-i][lastY-i] == _board[lastX-i+1][lastY-i+1] && _board[lastX-i][lastY-i] == _board[lastX-i+2][lastY-i+2] && _board[lastX-i][lastY-i] == _board[lastX-i+3][lastY-i+3])
-                return true;
+        // vertical check
+        for (int x = 0; x < COLS; x++){
+            for (int y = 0; y + 3 < ROWS; y++){
+                if (_board[x][y] != Color.Empty && _board[x][y] == _board[x][y+1] && _board[x][y] == _board[x][y+2] && _board[x][y] == _board[x][y+3])
+                    return true;
+            }
+        }
 
-        // diagonal (left-down to right-up) check
-        for (int i = Math.min(3, Math.min(ROWS-lastX,lastY)); i >= 0 && lastX + i - 3 < ROWS && lastY - i + 3 < COLS; i--)
-            if (_board[lastX+i][lastY-i] == _board[lastX+i-1][lastY-i-1] && _board[lastX+i][lastY-i] == _board[lastX+i-2][lastY-i+2] && _board[lastX-i][lastY-i] == _board[lastX+i-3][lastY-i+3])
-                return true;
+        // diagonal (up-right) check
+        for (int x = 0; x + 3 < COLS; x++){
+            for (int y = 0; y + 3 < ROWS; y++){
+                if (_board[x][y] != Color.Empty && _board[x][y] == _board[x+1][y+1] && _board[x][y] == _board[x+2][y+2] && _board[x][y] == _board[x+3][y+3])
+                    return true;
+            }
+        }
+
+        // diagonal (up-left) check
+        for (int x = 0; x + 3 < COLS; x++){
+            for (int y = ROWS-1; y > 0; y--){
+                if (_board[x][y] != Color.Empty && _board[x][y] == _board[x+1][y-1] && _board[x][y] == _board[x+2][y-2] && _board[x][y] == _board[x+3][y-3])
+                    return true;
+            }
+        }
 
         return false;
     }
